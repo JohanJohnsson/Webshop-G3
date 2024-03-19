57713afe8c72affb8d3776dd86c9e23265298ed0
AndreasHolmberg <145834751+Anho87@users.noreply.github.com>
2024-03-19 12:29:41 +0100

    test

--- js/scripts.js
+++ js/scripts.js
@@ -49,3 +49,74 @@ function validateForm() {
 
     return true;
 }
+
+fetch("https://fakestoreapi.com/products/category/electronics")
+  .then((res) => res.json())
+  .then((json) => {
+    json.forEach((item) => {
+        try{
+          createCard(item);
+    } catch (error) {
+  
+    }
+    });
+  });
+
+
+
+  function createCard(product) {
+    const id = product.id;
+    const title = product.title;
+    const price = "Price: " + product.price + "$";
+    //const category = product.category
+    const description = product.description;
+    const image = product.image;
+  
+    const cardDiv = document.createElement("div");
+    cardDiv.classList.add("card");
+    cardDiv.classList.add("my-2");
+    cardDiv.style.width = "18rem";
+  
+    const img = document.createElement("img");
+    img.classList.add("card-img-top");
+    img.src = image;
+    img.alt = `image describing ${title}`;
+    img.style.height = "300px";
+  
+    const cardBodyDiv = document.createElement("div");
+    cardBodyDiv.classList.add("card-body");
+    cardBodyDiv.classList.add("d-flex");
+    cardBodyDiv.classList.add("flex-column");
+  
+    const cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = title.slice(0, 50);
+  
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    cardText.textContent = description.slice(0, 100) + "...";
+  
+    const cardPrice = document.createElement("p");
+    cardPrice.classList.add("card-text");
+    cardPrice.classList.add("text-warning");  
+    cardPrice.textContent = price;
+  
+    const btn = document.createElement("a");
+    btn.classList.add("btn", "btn-success", "mt-auto");
+    btn.id = "addToCartId" + id;
+    btn.href = "#";
+    btn.textContent = "Add to cart";
+      
+  
+    cardBodyDiv.appendChild(cardTitle);
+    cardBodyDiv.appendChild(cardText);
+    cardBodyDiv.appendChild(cardPrice);
+    cardBodyDiv.appendChild(btn);
+  
+    cardDiv.appendChild(img);
+    cardDiv.appendChild(cardBodyDiv);
+  
+  
+    const card = document.querySelector('#products');
+    card.appendChild(cardDiv);
+  }
